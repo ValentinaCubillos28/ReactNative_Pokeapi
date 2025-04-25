a2796dae8cf281d63f9a4551f09f463efc380b09
Valentina Cubillos <lauracubi1063@gmail.com>
2025-04-25 18:51:36 +0000

    cambio 1

---
+++ assets/src/Componentes/Lista.js
@@ -0,0 +1,99 @@
+
+import { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, ScrollView, Image, TextInput } from 'react-native';
+import Filtro from './filtro'
+
+const [data, setData] = useState([]);
+const [busqueda, setBusqueda] = useState('');
+const [tipoSeleccionado, setTipoSeleccionado] = useState('All');
+const handleTipoChange = (tipo) => {
+    setTipoSeleccionado(tipo);
+    };
+   
+useEffect(() => {
+    const obtenerDatos = async () => {
+        if (tipoSeleccionado === 'All') {
+            const res = await
+                fetch("https://pokeapi.co/api/v2/pokemon?limit=1025");
+            const json = await res.json();
+            setData(json.results);
+        } else {
+            const res = await
+                fetch(`https://pokeapi.co/api/v2/type/${tipoSeleccionado}`);
+            const json = await res.json();
+            const listaFiltrada = json.pokemon.map(p => p.pokemon);
+            setData(listaFiltrada);
+        }
+    };
+    obtenerDatos();
+}, [tipoSeleccionado]);
+
+let resultados = data;
+if (busqueda.length >= 3 && isNaN(busqueda)) {
+    resultados = data.filter(pokemon =>
+        pokemon.name.toLowerCase().includes(busqueda.toLowerCase())
+    );
+} else if (!isNaN(busqueda) && busqueda !== '') {
+    resultados = data.filter(pokemon =>
+        pokemon.url.includes('/' + busqueda)
+    );
+}
+
+
+export default function Lista() {
+    return (
+
+        <ScrollView>
+            <TextInput
+                style={styles.buscador}
+                placeholder="Buscar Pokémon"
+                value={busqueda}
+                onChangeText={setBusqueda}
+            />
+            <Filtro onTipoChange={setTipoSeleccionado} />
+
+            <View style={styles.lista}>
+                {data.map((pokemon, index) => (
+                    <View key={index} style={styles.item}>
+                        <Text>{pokemon.url.split("/")[6]}</Text>
+                        <Image
+                            source={{
+                                uri:
+                                    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.url.split("/")[6]}.png`
+                            }}
+                            style={styles.imagen}
+                        />
+                        <Text>{pokemon.name}</Text>
+                    </View>
+                ))}
+            </View>
+        </ScrollView>
+    );
+}
+const styles = StyleSheet.create({
+    lista: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        gap: 10,
+        justifyContent: 'space-between', // para distribuir en 2 columnas
+        padding: 10,
+    },
+    item: {
+        backgroundColor: 'aliceblue',
+        width: '48%', // equivalente a calc(50% - 5px)
+        padding: 10,
+        alignItems: 'center',
+        marginBottom: 10,
+    },
+    imagen: {
+        width: 100,
+        height: 100,
+        resizeMode: 'contain',
+    },
+    buscador: {
+        margin: 10,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+    },
+});
